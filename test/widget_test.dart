3fe000d2b5887771116a0b1cf34736bbf826fa62
萍瑶 <pingping.lpp@alibaba-inc.com>
2019-05-20 11:25:13 +0800

    测试dartui

---
+++ test/widget_test.dart
@@ -0,0 +1,17 @@
+//import 'package:tmreader_flutter/widgets/title_bar.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // with Widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the Widget tell the tester to build it
+//    await tester.pumpWidget(TitleBar('T'));
+
+    // Create our Finders
+//    final titleFinder = find.text('T');
+
+    // Use the `findsOneWidget` matcher provided by flutter_test to verify our
+    // Text Widgets appear exactly once in the Widget tree
+//    expect(titleFinder, findsOneWidget);
+  });
+}
